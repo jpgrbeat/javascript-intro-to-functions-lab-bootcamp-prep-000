8421cfab0b044948be0813343a0dc1f5b9163134
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-05-07 17:28:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(aStr){
+  return aStr.toUpperCase();
+}
+function whisper(aStr){
+  return aStr.toLowerCase();
+}
+function logShout(aStr){
+  console.log(aStr.toUpperCase());
+}
+function logWhisper(aStr){
+  console.log(aStr.toLowerCase());
+}
+function sayHiToGrandma(aStr){
+  if(aStr.toUpperCase() === aStr){
+    return "YES, INDEED!"
+  }
+  if(aStr.toLowerCase() === aStr){
+    
+  }
+}
